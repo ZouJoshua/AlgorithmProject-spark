38074bcceb962b4fce16489ace70df9d43af1cd7
ZouJoshua <joshua_zou@163.com>
2019-01-10 00:13:49 +0800

    add entertainment data process

--- src/main/scala/com/apus/nlp/NewsSubCategory.scala
+++ src/main/scala/com/apus/nlp/NewsSubCategory.scala
@@ -100,44 +100,45 @@ object NewsSubCategory {
       val nonull_df = df.drop("_class", "_id", "article_doc_id", "is_right", "op_time", "server_time").filter("one_level = 'national'").filter("three_level is not null")
       val class_df = nonull_df.join(ori_df, Seq("article_id"))
       val crime_df = {
-        class_df.filter("two_level = 'crime' or three_level = 'crime'").drop("two_level").withColumn("two_level", lit("crime"))
+        class_df.filter("two_level = 'crime' or three_level = 'crime'")
+          .drop("two_level")
+          .withColumn("two_level", lit("crime"))
           .select("article_id", "title", "content", "one_level", "two_level", "three_level")
       }
       val law_df = {
-        class_df.filter("two_level = 'law' or three_level = 'law'").drop("two_level").withColumn("two_level", lit("law"))
+        class_df.filter("two_level = 'law' or three_level = 'law'")
+          .drop("two_level")
+          .withColumn("two_level", lit("law"))
           .select("article_id", "title", "content", "one_level", "two_level", "three_level")
       }
       val politics_df = {
-        class_df.filter("two_level = 'politics' and  three_level != 'people or groups'")
-          .select("article_id", "title", "content", "one_level", "two_level", "three_level")
-      }
-      val people_df = {
-        class_df.filter("three_level = 'people or groups'").drop("two_level")
-          .withColumn("two_level", lit("people or groups"))
-          .select("article_id", "title", "content", "one_level", "two_level", "three_level")
+        class_df.filter("two_level = 'politics'")
+          .select("article_id", "title", "content", "one_level", "two_level", "three_level").limit(13000)
       }
       val education_df = {
         class_df.filter("two_level = 'education'")
-          .select("article_id", "title", "content", "one_level", "two_level", "three_level")
+          .select("article_id", "title", "content", "one_level", "two_level", "three_level").limit(13000)
       }
       val others_df1 = {
         val nameUDF = udf((t: String) => if (t != null) t else "others")
         class_df.filter("two_level = 'society' and three_level in ('others','accidents','public benefit','events','food security')")
           .drop("two_level")
-          .withColumn("two_level", nameUDF(col("three_level")))
+          .withColumn("two_level", lit("society others"))
           .select("article_id", "title", "content", "one_level", "two_level", "three_level")
       }
       others_df1.cache
       val others_df2 = {
         class_df.filter("two_level in ('environment','medical','military', 'traffic')")
+          .drop("two_level")
+          .withColumn("two_level", lit("society others"))
           .select("article_id", "title", "content", "one_level", "two_level", "three_level")
       }
       others_df2.cache
-      val all_main = crime_df.union(law_df).union(politics_df).union(people_df).union(education_df).select("article_id", "title", "content", "one_level", "two_level", "three_level")
+      val all_main = crime_df.union(law_df).union(politics_df).union(education_df).select("article_id", "title", "content", "one_level", "two_level", "three_level")
       val others = others_df1.union(others_df2).select("article_id", "title", "content", "one_level", "two_level", "three_level")
       val all = all_main.union(others).distinct()
       println(">>>>>>>>>>正在写入数据")
-      all.coalesce(1).write.format("json").mode("overwrite").save("news_content/sub_classification/national/national_all")
+      all.coalesce(1).write.format("json").mode("overwrite").save("news_content/sub_classification/national/national_all_v1")
       println(">>>>>>>>>>写入数据完成")
     }
 
@@ -158,7 +159,20 @@ object NewsSubCategory {
       }
       val nonull_df = df.drop("_class", "_id", "article_doc_id", "is_right", "op_time", "server_time").filter("one_level = 'entertainment'")
       val class_df = nonull_df.join(ori_df, Seq("article_id"))
-
+      val all_main = {
+        class_df.filter("two_level in ('bollywood','celebrity','tv','movie','music','comic','hollywood')")
+          .select("article_id", "title", "content", "one_level", "two_level", "three_level")
+      }
+      val others = {
+        class_df.filter("two_level in ('art+culture+history','cartoon&comics','dance', 'others','variety show','performance')")
+          .drop("two_level")
+          .withColumn("two_level", lit("others"))
+          .select("article_id", "title", "content", "one_level", "two_level", "three_level")
+      }
+      val all = all_main.union(others).distinct()
+      println(">>>>>>>>>>正在写入数据")
+      all.coalesce(1).write.format("json").mode("overwrite").save("news_content/sub_classification/entertainment/entertainment_all")
+      println(">>>>>>>>>>写入数据完成")
     }
   }
 }
