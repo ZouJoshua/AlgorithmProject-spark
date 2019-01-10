ad95d24696d0cbeac260aae9ebda06f18ed98f4b
zoushuai <zoushuai@git.apuscn.com>
2019-01-10 21:58:41 +0800

    add sub category process

--- src/main/scala/com/apus/nlp/NewsSubCategory.scala
+++ src/main/scala/com/apus/nlp/NewsSubCategory.scala
@@ -87,8 +87,7 @@ object NewsSubCategory {
     //  1.去除three_level 为null
     //  2.抽取three_level 和two_level 为crime、law
     //  3.抽取two_level 为politics、education
-    //  4.politics 的people or groups 与 society 的 people or groups 合并
-    //  4.其他放为others(two_level: environment\medical\military\traffic\national economy
+    //  4.其他放为others(two_level: environment\medical\military\traffic\
     //        three_level: accidents\public benefit\events\others)
     def national_data_processer_v3(spark: SparkSession,
                                    newsPath: String,
@@ -96,8 +95,8 @@ object NewsSubCategory {
       val path = "/user/hive/warehouse/apus_ai.db/recommend/article/readmongo/dt=%s".format(dt)
       val df = spark.read.parquet(path)
       val getcontentUDF = udf { (html: String) => Jsoup.parse(html).text() }
-      val ori_df = spark.read.option("basePath", newsPath).parquet("/user/hive/warehouse/apus_dw.db/dw_news_data_hour/dt=2018-11-2[2-6]").selectExpr("resource_id as article_id", "html", "title").withColumn("content", getcontentUDF(col("html"))).drop("html")
-      val nonull_df = df.drop("_class", "_id", "article_doc_id", "is_right", "op_time", "server_time").filter("one_level = 'national'").filter("three_level is not null")
+      val ori_df = spark.read.option("basePath", newsPath).parquet("/user/hive/warehouse/apus_dw.db/dw_news_data_hour/dt=2018-11-2[2-6]").selectExpr("resource_id as article_id", "html", "title").withColumn("content", getcontentUDF(col("html"))).drop("html").dropDuplicates("article_id")
+      val nonull_df = df.drop("_class", "_id", "article_doc_id", "is_right", "op_time", "server_time").filter("one_level = 'national'").filter("three_level is not null").dropDuplicates("article_id")
       val class_df = nonull_df.join(ori_df, Seq("article_id"))
       val crime_df = {
         class_df.filter("two_level = 'crime' or three_level = 'crime'")
@@ -113,24 +112,24 @@ object NewsSubCategory {
       }
       val politics_df = {
         class_df.filter("two_level = 'politics'")
-          .select("article_id", "title", "content", "one_level", "two_level", "three_level").limit(13000)
+          .select("article_id", "title", "content", "one_level", "two_level", "three_level")
       }
       val education_df = {
         class_df.filter("two_level = 'education'")
-          .select("article_id", "title", "content", "one_level", "two_level", "three_level").limit(13000)
+          .select("article_id", "title", "content", "one_level", "two_level", "three_level")
       }
       val others_df1 = {
         val nameUDF = udf((t: String) => if (t != null) t else "others")
         class_df.filter("two_level = 'society' and three_level in ('others','accidents','public benefit','events','food security')")
           .drop("two_level")
-          .withColumn("two_level", lit("society others"))
+          .withColumn("two_level", lit("others"))
           .select("article_id", "title", "content", "one_level", "two_level", "three_level")
       }
       others_df1.cache
       val others_df2 = {
         class_df.filter("two_level in ('environment','medical','military', 'traffic')")
           .drop("two_level")
-          .withColumn("two_level", lit("society others"))
+          .withColumn("two_level", lit("others"))
           .select("article_id", "title", "content", "one_level", "two_level", "three_level")
       }
       others_df2.cache
@@ -148,6 +147,7 @@ object NewsSubCategory {
     def entertainment_data_processer_v1(spark: SparkSession,
                                    newsPath: String,
                                    dt: String = "2019-01-09") = {
+      val gossip_check_path = "news_content/sub_classification/entertainment/gossip_check"
       val path = "/user/hive/warehouse/apus_ai.db/recommend/article/readmongo/dt=%s".format(dt)
       val df = spark.read.parquet(path)
       val getcontentUDF = udf { (html: String) => Jsoup.parse(html).text() }
@@ -157,14 +157,23 @@ object NewsSubCategory {
           .withColumn("content", getcontentUDF(col("html")))
           .drop("html")
       }
-      val nonull_df = df.drop("_class", "_id", "article_doc_id", "is_right", "op_time", "server_time").filter("one_level = 'entertainment'")
+      val nonull_df = df.select("article_id", "one_level", "two_level", "three_level").filter("one_level = 'entertainment'").dropDuplicates("article_id")
       val class_df = nonull_df.join(ori_df, Seq("article_id"))
+      val gossip_check_df = spark.read.json(gossip_check_path).dropDuplicates("article_id")
+      val mark = gossip_check_df.withColumn("mark",lit(1)).select("article_id","mark")
+      val res = class_df.join(mark,Seq("article_id"),"left").filter("mark is null").drop("mark").select("article_id", "title", "content", "one_level", "two_level", "three_level")
+      val gossip_df = gossip_check_df.join(ori_df, Seq("article_id")) .select("article_id", "title", "content", "one_level", "two_level", "three_level")
+      val correct_df = gossip_df.union(res).distinct()
       val all_main = {
-        class_df.filter("two_level in ('bollywood','celebrity','tv','movie','music','comic','hollywood')")
-          .select("article_id", "title", "content", "one_level", "two_level", "three_level")
+        val renameUDF = udf{(t:String) => t.replace("celebrity", "celebrity&gossip").replace("cartoon&comics","comic").replace("comics","comic").replace("comic","cartoon&comics")}
+        correct_df.filter("two_level in ('bollywood','celebrity','tv','movie','music','comic','comics','hollywood','cartoon&comics')")
+            .withColumn("two_level_new", renameUDF(col("two_level")))
+            .drop("two_level")
+            .withColumnRenamed("two_level_new", "two_level")
+            .select("article_id", "title", "content", "one_level", "two_level", "three_level")
       }
       val others = {
-        class_df.filter("two_level in ('art+culture+history','cartoon&comics','dance', 'others','variety show','performance')")
+        correct_df.filter("two_level in ('art+culture+history','dance', 'others','variety show','performance')")
           .drop("two_level")
           .withColumn("two_level", lit("others"))
           .select("article_id", "title", "content", "one_level", "two_level", "three_level")
