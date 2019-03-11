606c7d477763e6f86bc81d98aee9daff4ecea5f2
zoushuai <zoushuai@git.apuscn.com>
2019-03-11 19:46:57 +0800

    update sub_classification

--- src/main/scala/com/apus/mongodb/CMS2MarkTestData.scala
+++ src/main/scala/com/apus/mongodb/CMS2MarkTestData.scala
@@ -3,6 +3,8 @@ package com.apus.mongodb
 import org.apache.spark.sql.{SaveMode, SparkSession}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
+import org.jsoup.Jsoup
+import org.jsoup.nodes.{Element, TextNode}
 
 
 /**
@@ -35,9 +37,9 @@ object CMS2MarkTestData {
     //
 
     val dt = "2019-03-08"
+//    val inputPath = "/user/zoushuai/news_content/cms2marktest"
     val inputPath = "/user/zoushuai/news_content/cms2marktestdata"
     val outputUri = "mongodb://127.0.0.1:27017/news.article_info_doc"
-
 
     val  keywordType = {
       new StructType()
@@ -52,6 +54,28 @@ object CMS2MarkTestData {
         .add("status",StringType)
         .add("title_keywords",ArrayType(keywordType))
         .add("content_keywords",ArrayType(keywordType))
+    }
+
+
+    val structSchema =
+      StructType(Seq(
+        StructField("review_status",ArrayType(IntegerType)),
+        StructField("category",StringType),
+        StructField("sub_category",StringType),
+        StructField("status",StringType),
+        StructField("tags", ArrayType(StructType(Seq(StructField("weight", DoubleType),StructField("keyword", StringType)
+        )))),
+        StructField("title_keywords", ArrayType(StructType(Seq(StructField("weight", DoubleType),StructField("keyword", StringType)
+        )))),
+        StructField("content_keywords", ArrayType(StructType(Seq(StructField("weight", DoubleType), StructField("keyword", StringType)
+        ))))
+      ))
+
+
+    val nlp_keywordType = {
+      new StructType()
+        .add("weight",DoubleType)
+        .add("keyword",StringType)
     }
 
     val accountType = {
@@ -73,16 +97,71 @@ object CMS2MarkTestData {
         .add("origin",originType)
     }
 
+    val share_linkType = {
+      new StructType()
+        .add("1", StringType)
+        .add("2", StringType)
+        .add("4", StringType)
+    }
+
+    val extraType = {
+      new StructType()
+        .add("_id",StringType)
+    }
+
+
+    // 解析html，text加上apus标签后，再拼接成html
+    val tagMarkUDF = udf{
+      (html:String,keywords:Seq[String]) =>
+        var tag_content = " "+html+" "
+        if(keywords.nonEmpty){
+          //          Entities.EscapeMode.base.getMap().clear()
+          val doc = Jsoup.parse(tag_content)
+          val allElements = doc.body().getAllElements.toArray.map(_.asInstanceOf[Element])
+          for(i <- allElements){
+            val tnList = i.textNodes().toArray().map(_.asInstanceOf[TextNode])
+            for(tn <- tnList) {
+              var ori_text = " " + tn.text + " "
+              keywords.foreach{
+                w =>
+                  // 有一些特殊的词带有符号的, f***ing n****r tl;dr 等
+                  val w_clean = w.map{
+                    a=>
+                      if("!\"$()*+.[]\\^{}|".contains(a)){
+                        "\\" + a
+                        //                        "\\\\" + a
+                      } else a
+                  }.mkString("")
+                  // 匹配 '>','<'里面的内容，防止将html标签里的内容替换掉
+                  val toReplace_ = "<i class=\"apus-entity-words\"> "+w+" </i>"
+                  val toReplace = java.util.regex.Matcher.quoteReplacement(toReplace_)
+                  ori_text = ori_text.replaceAll("(?<=[^\\p{L}])("+w_clean+")(?=([^\\p{L}]|‘s|`s|'s))", toReplace)
+                //                  ori_text = ori_text.replaceAll("(?<=[^\\p{L}])(?i)("+w_clean+")(?-i)(?=[^\\p{L}])","<i class=\"apus-entity-words\"> "+w+" </i>")
+              }
+              tn.text(ori_text)
+            }
+          }
+          tag_content = doc.body().children().toString.replace("&lt;i class=\"apus-entity-words\"&gt;","<i class=\"apus-entity-words\">").replace("&lt;/i&gt;","</i>")
+          tag_content
+        } else {tag_content = html}
+        tag_content
+    }
+
+
     val saveDf = {
       spark.read.parquet(inputPath)
-        .withColumnRenamed("algo_profile", "algo_profile_old")
-        .withColumnRenamed("images","images_old")
-        .withColumnRenamed("account","account_old")
-        .withColumn("account",from_json(col("account_old"),accountType))
-        .withColumn("images",from_json(col("images_old"), imagesType))
-        .withColumn("algo_profile",from_json(col("algo_profile_old"),algo_profileType))
-        .selectExpr("inner_type","share_link", "rqstid","url","title","country","lang","source","summary","introduction","buss_type",
-          "generate_url","account","resource_id","article","algo_profile","images","repeat_content",
+        .withColumn("nlp_keywords_new", from_json(col("nlp_keywords"),ArrayType(nlp_keywordType)))
+        .withColumn("account_new",from_json(col("account"), accountType))
+        .withColumn("images_new",from_json(col("images"), ArrayType(imagesType)))
+        .withColumn("algo_profile_new",from_json(col("algo_profile"),algo_profileType))
+        .withColumn("share_link_new", from_json(col("share_link"), share_linkType))
+        .withColumn("buss_type", lit(0))
+//        .withColumn("extra_new", from_json(col("extra"), extraType))
+        .selectExpr("inner_type", "rqstid","url","title","country","lang","source","summary","introduction","buss_type",
+          "generate_url","account","resource_id","article",
+          "algo_profile_new as algo_profile","images_new as images",
+          "repeat_content","nlp_keywords_new as nlp_keywords",
+          "share_link_new as share_link", "extra",
           "cast(category as int) category", "cast(sub_class as int) sub_class",
           "cast(sub_category as int) sub_category","cast(third_category as int) third_category",
           "cast(create_time as long) create_time","cast(pub_time as long) pub_time",
@@ -93,15 +172,27 @@ object CMS2MarkTestData {
           "cast(is_hotnews as int) is_hotnews","cast(is_newarticle as int) is_newarticle",
           "cast(is_partner as int) is_partner", "cast(origin_category as int) origin_category",
           "cast(source_id as long) source_id")
-
     }
 
+    val article_process = saveDf.select("resource_id","article.html","algo_profile.content_keywords.word")
+    val article_new = article_process.withColumn("html_with_tag", tagMarkUDF(col("html"),col("word"))).drop("word","html")
 
+    val result = saveDf.join(article_new, Seq("resource_id")).withColumnRenamed("article", "article_new").withColumnRenamed("algo_profile","algo_profile_new")
+    val articleStructCols = result.select("article_new.*").columns.filter(_!="html").map(name => col("article_new."+name))
+    val algo_profileStructCols = result.select("algo_profile_new.*").columns.map(name => col("algo_profile_new."+name))
 
-    print(saveDf.printSchema())
-    val num = saveDf.count
+    val result_new = {
+      result.withColumn("algo_profile",struct((algo_profileStructCols:+col("algo_profile_new.title_keywords").as("tags")):_*))
+          .withColumn("article", struct((articleStructCols:+col("html_with_tag").as("html")):_*)).drop("article_new","algo_profile_new","html_with_tag")
+        }
+
+    val profileDf = result_new.select(col("algo_profile").cast(structSchema),col("resource_id"))
+
+    val out = result_new.drop("algo_profile").join(profileDf, Seq("resource_id"))
+    print(out.printSchema())
+    val num = out.count
     // 写入mongodb
-    saveDf.write.options(Map("spark.mongodb.output.uri" -> outputUri))
+    out.write.options(Map("spark.mongodb.output.uri" -> outputUri))
 //      .mode("append")
             .mode("overwrite")
       .format("com.mongodb.spark.sql")
