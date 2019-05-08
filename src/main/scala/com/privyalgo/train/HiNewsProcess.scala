210064c91abe5b017df4bff42e87c440a5e33274
zs <joshua_zou@163.com>
2019-05-08 17:36:58 +0800

    add hi news process

---
+++ src/main/scala/com/privyalgo/train/HiNewsProcess.scala
@@ -0,0 +1,40 @@
+package com.privyalgo.train
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, udf}
+import org.jsoup.Jsoup
+
+/**
+  * Created by Joshua on 19-5-8
+  */
+
+
+object HiNewsProcess {
+  def main(args: Array[String]): Unit = {
+
+    val appName = "Hi-News-Process"
+    val spark = SparkSession.builder().appName(appName).getOrCreate()
+    val sc = spark.sparkContext
+
+    //***********//
+    //印度语新闻处理//
+    //***********//
+    val hi_category_path = "hi_news/result_topcategory_all"
+    val hi_news_path = "/user/hive/warehouse/apus_ai.db/recommend/common/news_hindi_parsed"
+    val hi_category_out_path = "hi_news/topcategory_all"
+    val getcontentUDF = udf{ html: String => Jsoup.parse(html).text()}
+    val hi_df = {
+      spark.read.parquet(hi_news_path)
+        .filter("country = 'IN' and lang = 'hi'")
+        .selectExpr("resource_id as id", "url", "title", "article.html as html", "category")
+        .withColumn("content", getcontentUDF(col("html")))
+        .drop("html")
+    }
+
+    val hi_category_df = spark.read.json(hi_category_path).selectExpr("id","top_category","result.tag as tags")
+
+    val hi_category = hi_category_df.join(hi_df,Seq("id"))
+    hi_category.coalesce(1).write.format("json").mode("overwrite").save(hi_category_out_path)
+
+  }
+}
