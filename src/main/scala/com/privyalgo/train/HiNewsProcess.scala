5ac370539b2db62bb7595066c293de227f62010c
zs <joshua_zou@163.com>
2019-05-15 15:06:07 +0800

    add hi news process

--- src/main/scala/com/privyalgo/train/HiNewsProcess.scala
+++ src/main/scala/com/privyalgo/train/HiNewsProcess.scala
@@ -19,9 +19,14 @@ object HiNewsProcess {
     //***********//
     //印度语新闻处理//
     //***********//
-    val hi_category_path = "hi_news/result_topcategory_all"
+
     val hi_news_path = "/user/hive/warehouse/apus_ai.db/recommend/common/news_hindi_parsed"
-    val hi_category_out_path = "hi_news/topcategory_all"
+    // 解析分类获取的top—category
+//    val hi_category_path = "hi_news/result_topcategory_all"
+//    val hi_category_out_path = "hi_news/topcategory_all"
+    // 解析网址获取的top-category
+    val hi_category_path = "hi_news/result_topcategory_website_all"
+    val hi_category_out_path = "hi_news/topcategory_website_all"
     val getcontentUDF = udf{ html: String => Jsoup.parse(html).text()}
     val hi_df = {
       spark.read.parquet(hi_news_path)
@@ -31,10 +36,21 @@ object HiNewsProcess {
         .drop("html")
     }
 
-    val hi_category_df = spark.read.json(hi_category_path).selectExpr("id","top_category","result.tag as tags")
+//    val hi_category_df = spark.read.json(hi_category_path).selectExpr("id","top_category","result.tag as tags")
+    val hi_category_df = spark.read.json(hi_category_path).selectExpr("id","top_category")
 
     val hi_category = hi_category_df.join(hi_df,Seq("id"))
     hi_category.repartition(1).write.format("json").mode("overwrite").save(hi_category_out_path)
+
+    //*************************//
+    //单独处理tech、auto、science//
+    //*************************//
+    val df1 = spark.read.json("hi_news/result_topcategory_all").filter("top_category in ('technology', 'auto', 'science')").select("id", "top_category")
+    val df2 = spark.read.json("hi_news/topcategory_website_all").filter("top_category in ('technology', 'auto', 'science')").select("id", "top_category")
+
+    val df_union = df1.union(df2).distinct().dropDuplicates("id")
+    val hi_df_less =df_union.join(hi_df,Seq("id"))
+    hi_df_less.repartition(1).write.format("json").mode("overwrite").save("hi_news/auto_science_tech")
 
   }
 }
